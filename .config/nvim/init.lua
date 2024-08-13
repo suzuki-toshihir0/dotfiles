7f35d5a28ab215b322d8181c8f18049492d610af
Toshihiro Suzuki <suzuki@arkedgespace.com>
2024-08-13 20:58:57 +0900

    [nvim] introduce nvim-cmp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -114,6 +114,60 @@ vim.cmd('syntax enable')
 vim.cmd('filetype plugin indent on')
 vim.g.rustfmt_autosave = 1
 
+-- nvim-cmp setup
+local cmp = require'cmp'
+
+-- Global setup.
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'snippy' }, -- For snippy users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- `/` cmdline setup.
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- `:` cmdline setup.
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  }),
+  matching = { disallow_symbol_nonprefix_matching = false }
+})
+
 -- LSP config
 -- It's important that you set up the plugins in the following order:
 --
@@ -146,6 +200,12 @@ require("mason-lspconfig").setup_handlers {
             }
           }
         }
+      }
+    end,
+    ["clangd"] = function()
+      require("lspconfig").clangd.setup{
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+        cmd = { "clangd", "--offset-encoding=utf-16" },
       }
     end
 }
@@ -208,3 +268,4 @@ vim.keymap.set("n", "gn", "<cmd>Lspsaga rename<CR>")
 vim.keymap.set("n", "ge", "<cmd>Lspsaga show_line_diagnostics<CR>")
 vim.keymap.set("n", "[e", "<cmd>Lspsaga diagnostic_jump_next<CR>")
 vim.keymap.set("n", "]e", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
+
